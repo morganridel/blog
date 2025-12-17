a6943e767cb68847515650a5c2b7ec220b03ed02
Morgan Ridel <=>
2025-12-17 16:04:13 +0100

    migrate articles

--- lib/blog/builder.ex
+++ lib/blog/builder.ex
@@ -1,26 +1,62 @@
 defmodule Blog.Builder do
   alias Blog.{ContentReader, HTMLGenerator}
 
+  defp posts_dir(), do: Application.get_env(:blog, :posts_dir)
+
   def run do
     output_dir = Application.get_env(:blog, :output_dir)
 
     File.rm_rf!(output_dir)
     File.mkdir_p!(output_dir <> "/static")
 
-    posts = ContentReader.read_all_posts()
-    IO.puts("📝 Found #{length(posts)} posts.")
+    contents = ContentReader.read_all_posts()
+    IO.puts("📝 Found #{length(contents)} contents.")
+    IO.inspect(contents)
 
-    Enum.each(posts, fn post ->
-      html = HTMLGenerator.generate_post_html(post)
-      File.write!(Path.join(output_dir, "#{post.slug}.html"), html)
+    Enum.each(contents, fn content ->
+      process_content(content)
     end)
 
-    index_html = HTMLGenerator.generate_index_html(posts)
+    index_html =
+      HTMLGenerator.generate_index_html(
+        contents
+        |> Enum.filter(&match_posts?/1)
+        |> Enum.map(fn {:post, post} -> post end)
+        |> Enum.sort_by(& &1.frontmatter.date, {:desc, Date})
+      )
+
     File.write!(Path.join(output_dir, "index.html"), index_html)
 
     copy_assets(output_dir)
 
     IO.puts("✅ Build complete in /#{output_dir}")
+  end
+
+  defp match_posts?({:post, _}), do: true
+  defp match_posts?(_), do: false
+
+  defp process_content({:post, post}) do
+    output_dir = Application.get_env(:blog, :output_dir)
+    html = HTMLGenerator.generate_post_html(post)
+    file_path = Path.join([output_dir, post.path, "#{post.slug}.html"])
+
+    with :ok <- File.mkdir_p(Path.dirname(file_path)) do
+      File.write!(file_path, html)
+    end
+  end
+
+  defp process_content({:default, path}) do
+    output_dir = Application.get_env(:blog, :output_dir)
+    source_path = Path.join([posts_dir(), path])
+    file_path = Path.join([output_dir, path])
+
+    IO.puts("Processing content...")
+    IO.inspect(path)
+    IO.inspect(file_path)
+    # Just copy the file
+    with :ok <- File.mkdir_p(Path.dirname(file_path)) do
+      File.cp!(source_path, file_path)
+    end
   end
 
   defp copy_assets(output_dir) do
--- lib/blog/content_reader.ex
+++ lib/blog/content_reader.ex
@@ -2,15 +2,29 @@ defmodule Blog.ContentReader do
   @doc """
   Reads all files from the configured posts directory.
   """
-  def read_all_posts do
-    posts_dir = Application.get_env(:blog, :posts_dir)
 
-    posts_dir
-    |> File.ls!()
-    |> Enum.filter(&String.ends_with?(&1, ".md"))
-    |> Enum.map(&Path.join(posts_dir, &1))
-    |> Enum.map(&read_post/1)
-    |> Enum.sort_by(& &1.frontmatter.date, {:desc, Date})
+  defp posts_dir(), do: Application.get_env(:blog, :posts_dir)
+
+  def read_all_posts do
+    Path.wildcard(posts_dir() <> "/**")
+    |> Enum.filter(&File.regular?/1)
+    |> Enum.map(&parse_content/1)
+    |> Enum.reject(&(&1 == :ignore))
+  end
+
+  defp parse_content(path) do
+    ext = Path.extname(path)
+
+    cond do
+      ext in [".md", ".mdx"] ->
+        {:post, read_post(path)}
+
+      ext in [".html", ".jpg", ".png", ".jpeg", ".mp4"] ->
+        {:default, Path.relative_to(path, posts_dir())}
+
+      true ->
+        :ignore
+    end
   end
 
   defp read_post(file_path) do
@@ -21,26 +35,41 @@ defmodule Blog.ContentReader do
         extension: [shortcodes: true, front_matter_delimiter: "---"]
       )
 
-    slug = Path.basename(file_path, ".md")
+    slug = Path.basename(file_path, Path.extname(file_path))
+    path = Path.relative_to(file_path, posts_dir()) |> Path.dirname()
 
     %{
       slug: slug,
-      frontmatter: %{title: _, date: _, author: _} = parse_frontmatter(doc),
+      path: path,
+      frontmatter: parse_frontmatter(doc),
       html_body: doc |> MDEx.to_html!(),
-      url: "/#{slug}.html"
+      url:
+        case slug do
+          # TODO: make images in articles works without trailing slash? how is it handled on my old blog? and in Astro?
+          "index" -> path <> "/"
+          _ -> Path.join([path, "#{slug}.html"])
+        end
     }
   end
 
   defp parse_frontmatter(%MDEx.Document{} = doc) do
     %MDEx.FrontMatter{literal: text} = doc[MDEx.FrontMatter] |> List.first()
 
-    text
-    |> String.split("\n", trim: true)
-    |> Enum.reduce(%{}, fn line, acc ->
-      case String.split(line, ":", parts: 2) do
-        [key, val] -> Map.put(acc, String.to_atom(String.trim(key)), String.trim(val))
-        _ -> acc
-      end
-    end)
+    frontmatter =
+      text
+      |> String.split("\n", trim: true)
+      |> Enum.reduce(%{}, fn line, acc ->
+        case String.split(line, ":", parts: 2) do
+          [key, val] -> Map.put(acc, String.to_atom(String.trim(key)), String.trim(val))
+          _ -> acc
+        end
+      end)
+
+    %{title: frontmatter.title, date: parse_date!(frontmatter.date), author: frontmatter.author}
+  end
+
+  defp parse_date!(date) when is_binary(date) do
+    {:ok, date} = Date.from_iso8601(date)
+    date
   end
 end
